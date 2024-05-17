bc419dcae7277f632e9b52569930807aa43f2767
GabrielPaixao <gabrielpaixas@gmail.com>
2024-05-17 12:47:08 -0300

    nome na verificação

--- teste.py
+++ teste.py
@@ -26,6 +26,14 @@ def cadastrar_rosto(nome, pasta='rostos'):
 def verificar_rosto(pasta='rostos'):
     cap = cv2.VideoCapture(0)
     
+    # Dicionário para armazenar os rostos cadastrados
+    rostos_cadastrados = {}
+    for arquivo in os.listdir(pasta):
+        if arquivo.endswith(".npy"):
+            nome_rosto = os.path.splitext(arquivo)[0]  # Remover a extensão .npy para obter o nome do rosto
+            encode_salvo = np.load(os.path.join(pasta, arquivo))
+            rostos_cadastrados[nome_rosto] = encode_salvo
+    
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -38,22 +46,20 @@ def verificar_rosto(pasta='rostos'):
             encode_teste = fr.face_encodings(frame)
             if len(encode_teste) > 0:
                 encode_teste = encode_teste[0]
-                for arquivo in os.listdir(pasta):
-                    if arquivo.endswith(".npy"):
-                        encode_salvo = np.load(os.path.join(pasta, arquivo))
-                        comparador = fr.compare_faces([encode_salvo], encode_teste)
-                        if comparador[0]:
-                            print("Rosto cadastrado")
-                            break
-                    cap.release()        
+                for nome, encode_salvo in rostos_cadastrados.items():
+                    comparador = fr.compare_faces([encode_salvo], encode_teste)
+                    if comparador[0]:
+                        print("Rosto cadastrado:", nome)
+                        break
                 else:
-                    print("Rosto nao cadastrado")
+                    print("Rosto não cadastrado")
                     break
+                
     cap.release()
     cv2.destroyAllWindows()
         
     
-escolha = input("1. Cadastrar\n2. Verificar\n")
+escolha = input("\n1. Cadastrar\n2. Verificar\n")
 
 if escolha == "1":
     nome = input("Nome: ")
