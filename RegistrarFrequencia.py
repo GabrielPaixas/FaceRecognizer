2a32b998e4a8b3355e332d2edccbd438beb7b37e
GabrielPaixao <gabrielpaixas@gmail.com>
2024-06-07 15:26:15 -0300

    correçoes simples

--- RegistrarFrequencia.py
+++ RegistrarFrequencia.py
@@ -5,16 +5,13 @@ def registrar_frequencia(nome, pasta='frequencia'):
     if not os.path.exists(pasta):
         os.makedirs(pasta)
 
-    # Cria uma subpasta com o nome da pessoa
     subpasta = os.path.join(pasta, nome)
     if not os.path.exists(subpasta):
         os.makedirs(subpasta)
     
-    # Define o nome do arquivo com a data atual
     data_atual = datetime.datetime.now().strftime("%Y-%m-%d")
     arquivo = os.path.join(subpasta, f"{data_atual}.txt")
     
-    # Obtém o horário atual
     horario = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     if os.path.exists(arquivo):
@@ -23,7 +20,6 @@ def registrar_frequencia(nome, pasta='frequencia'):
             if len(linhas) == 0:
                 f.write(f"Entrada: {horario}\n")
             else:
-                # Atualiza a "Saída"
                 for i, linha in enumerate(linhas):
                     if "Saida" in linha:
                         linhas[i] = f"Saida: {horario}\n"
--- VerificarFace.py
+++ VerificarFace.py
@@ -7,11 +7,10 @@ from RegistrarFrequencia import registrar_frequencia
 def verificar_rosto(pasta='rostos'):
     cap = cv2.VideoCapture(0)
 
-    # Definir o número de quadros entre cada processamento
     quadros_entre_processamento = 5
     contador_quadros = 0
 
-    # Carregar todas as codificações salvas na memória
+
     encodings_salvas = []
     for arquivo in os.listdir(pasta):
         if arquivo.endswith(".npy"):
@@ -25,23 +24,21 @@ def verificar_rosto(pasta='rostos'):
 
         contador_quadros += 1
 
-        # Processar apenas a cada X quadros
         if contador_quadros % quadros_entre_processamento == 0:
-            # Reduzir a resolução do frame para acelerar o processamento
+
             pequena_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-            # Converte o frame reduzido para RGB (face_recognition usa RGB)
             rgb_pequena_frame = pequena_frame[:, :, ::-1]
 
-            # Detecta as localizações dos rostos
+
             face_locations = fr.face_locations(rgb_pequena_frame)
             print(f"Localizações de rostos: {face_locations}")
 
             if len(face_locations) > 0:
-                # Tirar uma série de frames para comparação
+
                 frames_para_comparar = [rgb_pequena_frame.copy() for _ in range(5)]
 
-                # Comparar os frames capturados com os rostos salvos
+
                 rostos_cadastrados = 0
                 for frame_comparar in frames_para_comparar:
                     encode_comparar = fr.face_encodings(frame_comparar)
@@ -56,12 +53,11 @@ def verificar_rosto(pasta='rostos'):
                                     rostos_cadastrados += 1
                                     break
 
-                # Se todos os 5 frames tiverem rostos cadastrados
                 if rostos_cadastrados == 5:
                     print("Entrada/Saida registrada")
                 else:
                     print("Rosto nao encontrado")
-        # Mostrar o frame original (não redimensionado)
+                    
         cv2.imshow('Pressione Q para sair', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
